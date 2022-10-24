fc49c8322f902318135fe20a3a4b0f6e724b55b3
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-24 20:43:35 +0300

    Сортировка массива по убыванию

---
+++ Lesson3-2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Упорядочение элементов массива по убыванию
+int[] arr = { 1, 8, 6, 5, 4, 9, 0, 2};
+PrintArray(arr);
+Console.WriteLine();
+SelectionSort(arr);
+PrintArray(arr);
+
+// Печать массива
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+
+//Упорядочивание массива
+void SelectionSort(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count-1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < count; j++)
+        {
+            if (array[j] > array[maxPosition])
+                maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
