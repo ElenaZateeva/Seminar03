19093ac48fc7805c5fe6bcd94884bc0193535ac5
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-24 17:24:02 +0300

    Редактирование текста. Замена в нем одних символов другими

---
+++ Lesson3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Дан текст. Заменить в нем пробелы на знак "|", "в" на "В", "Г" на "г"
+string text = "Однажды в студенную зимнюю пору "
+        + "Я из лесу вышел. Был сильный мороз. "
+        + "Гляжу, поднимается медленно в гору "
+        + "Лошадка, везущая хворосту воз.";
+
+// Метод замены символов
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue)
+            result = result + $"{newValue}";
+        else
+            result = result + $"{text[i]}";
+    }
+    return result;
+}
+    
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+string newText1 = Replase(newText, 'в', 'В');
+Console.WriteLine(newText1);
+Console.WriteLine();
+string newText2 = Replase(newText1, 'Г', 'г');
+Console.WriteLine(newText2);
