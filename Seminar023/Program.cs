1a26c29ac7aa4f1f4473ccb71c6cf1b9007656c4
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-25 22:04:21 +0300

    Расчитать значение кубов чисел от 1 до N

---
+++ Seminar023/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает на входе число N и выдает куб чисел от 1 до N
+
+Console.WriteLine("Введи число N");
+int n=Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i*i*i} ");
+}
