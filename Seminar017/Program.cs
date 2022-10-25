346c73dd7d5242b935c20a156b04a64b8be79260
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-25 09:47:35 +0300

    Задает координаты точки на плоскости, выдает номер четверти

---
+++ Seminar017/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа на входе получает координаты (x,y), при чем x<>0 и y<>0. 
+// На выходе выдает № четверти оси координат.
+
+Console.WriteLine("Введите по очереди координаты точки А x и y");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+int numberOfQuarter = DetermineNumberOfQuarter(x, y);
+Console.WriteLine(numberOfQuarter);
+
+int DetermineNumberOfQuarter(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return -1;
+}
+
+
